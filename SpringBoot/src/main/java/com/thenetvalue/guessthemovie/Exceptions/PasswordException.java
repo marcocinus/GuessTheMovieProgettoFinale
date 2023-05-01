67e813bf23271f9f458671a3f651f8b6ed82a70c
BasilBasilio <cristiano.aru@gmail.com>
2023-05-01 20:44:47 +0200

    Elimino le vecchie cartelle Springboot e Node
    Per preparare la repository abbiamo cancellato le cartelle di Springboot e Node

--- SpringBoot/src/main/java/com/thenetvalue/guessthemovie/Exceptions/PasswordException.java
+++
@@ -1,7 +0,0 @@
-package com.thenetvalue.guessthemovie.Exceptions;
-
-public class PasswordException extends RuntimeException{
-    public PasswordException(String message){
-        super(message);
-    }
-}
--- SpringBoot/src/main/java/com/thenetvalue/guessthemovie/Exceptions/UserExistingException.java
+++
@@ -1,7 +0,0 @@
-package com.thenetvalue.guessthemovie.Exceptions;
-
-public class UserExistingException extends RuntimeException{
-    public UserExistingException(String message){
-        super(message);
-    }
-}
